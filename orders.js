539eefb217daafe46b10e85789fac2061777ad22
Caleb Hodges <chodgesdev@gmail.com>
2022-12-02 18:57:00 -0600

    orders.js completed

--- orders.js
+++ orders.js
@@ -31,6 +31,18 @@
 
 //CODE HERE
 
+class Ticket {
+    constructor(items, orderTime, customerId) {
+        this.items = items
+        this.orderTime = customerId
+        this.customerId = customerId
+        this.status = "queued"
+    }
+    updateStatus(newStatus) {
+        this.status = newStatus
+        console.log(`The order for customer ${this.customerId} is now ${this.status}`)
+    }
+}
 
 
 /*
@@ -45,8 +57,8 @@
 */
 
 //CODE HERE
-
-
+const firstTicket = new Ticket("pizza, bread, soda", "7:03 PM", 575)
+console.log("firstTicket before update:", firstTicket)
 /*
     Call the `updateStatus` method on
     `firstTicket` passing in the string
@@ -54,3 +66,6 @@
 */
 
 //CODE HERE
+
+firstTicket.updateStatus("cooking")
+console.log("firstTicket after update:", firstTicket)
